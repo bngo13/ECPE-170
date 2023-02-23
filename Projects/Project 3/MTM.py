7492a239ebf3332c90094869b01be28fc63ea539
dalamity <mrslime43@gmail.com>
2023-02-23 09:52:31 -0800

    Added constants for R and I types

---
+++ Projects/Project 3/MTM.py
@@ -0,0 +1,13 @@
+
+# Set constants
+R_LIST = ["add", "sub", "sll", "srl", "slt"]
+R_BINARY = ["", "", "", "", ""]
+
+I_LIST = ["addi", "beq", "bne", "lw", "sw"]
+I_BINARY = ["001000", "000100", "000101", "100011", "101011"]
+
+def main():
+	
+
+if __name__ == "__main__":
+	main()
