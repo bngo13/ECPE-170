c297855cf10994fa55a4333e1b17aa7da0968cf5
Brain <dalamamommy@dalamamaggot.localdomain>
2023-02-24 12:49:09 -0800

    Added and completed parsing for rtype and itype

--- Projects/Project 3/MTM.py
+++ Projects/Project 3/MTM.py
@@ -1,13 +1,229 @@
+'''
+Add	: 000000	rs	rt	rd	sa	100000 	| sa = 00000
+Sub	: 000000	rs	rt	rd	sa	100010	| sa = 00000
+SLL	: 000000	rs	rt	rd	sa	000000		| ra = 00000
+SRL	: 000000	rs	rt	rd	sa	000010		| ra = 00000
+SLT	: 000000	rs	rt	rd	sa	101010	| sa = 00000
+
+Addi: 001000	rs	rt	imm
+BEQ	: 000100	rs	rt	offset
+BNE	: 000101	rs	rt	offset
+LW	: 100011	rs	rt	offset
+SW	: 101011	rs	rt	offset
+'''
+
 
 # Set constants
-R_LIST = ["add", "sub", "sll", "srl", "slt"]
-R_BINARY = ["", "", "", "", ""]
+R_LIST = [
+	"add", 
+	"sub", 
+	"sll", 
+	"srl", 
+	"slt"
+]
+R_BINARY = [
+	["000000", "", "", "", "", "100000"], 
+	["000000", "", "", "", "", "100010"], 
+	["000000", "", "", "", "", "000000"], 
+	["000000", "", "", "", "", "000010"], 
+	["000000", "", "", "", "", "101010"]
+]
 
-I_LIST = ["addi", "beq", "bne", "lw", "sw"]
-I_BINARY = ["001000", "000100", "000101", "100011", "101011"]
+I_LIST = [
+	"addi", 
+	"beq", 
+	"bne", 
+	"lw", 
+	"sw"
+]
+I_BINARY = [ # Append at beginning of binary string
+	["001000", "", "", ""], 
+	["000100", "", "", ""], 
+	["000101", "", "", ""], 
+	["100011", "", "", ""], 
+	["101011", "", "", ""]
+]
+
+REGISTER_LISTS = [
+	"zero",
+	"at",
+	"v0", "v1",
+	"a0", "a1", "a2", "a3",
+	"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+	"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+	"t8", "t9",
+	"k0", "k1",
+	"gp",
+	"sp",
+	"s8",
+	"ra"
+]
+
+
+# Start function defs
+def parse_instruction(input_string):
+	'''
+		OpCode
+	'''
+	instructionType = "rtype"
+	instructionToReg = input_string.split(" ", 1)
+	
+	# Check if input is RList or not
+	if not (instructionToReg[0] in R_LIST or instructionToReg[0] in I_LIST):
+		return ""
+	
+	template = parse_instruc_to_binary(instructionToReg[0])
+	if instructionToReg[0] in I_LIST:
+		instructionType = "itype"
+	
+	'''
+		Registers
+	'''
+	instructionToReg[1] = instructionToReg[1].replace(" ", "")
+	regList = instructionToReg[1].split(",")
+	
+	# Check for regList being the right size
+	if len(regList) != 3 and len(regList) != 2:
+		return ""
+		
+	if instructionType == "rtype":
+		if not parse_rtype(instructionToReg[0], template, regList):
+			return ""
+	else:
+		if not parse_itype(instructionToReg[0], template, regList):
+			return ""
+	print(template)
+	
+def parse_instruc_to_binary(stringin):
+	# stringin = instruction input
+	if stringin in R_LIST:
+		index = R_LIST.index(stringin)
+		return R_BINARY[index]
+	index = I_LIST.index(stringin)
+	return I_BINARY[index]
+
+def parse_rtype(opcode, template, registers):
+	# opcode: string of opcode
+	# template: list type
+	# registers: list of registers 
+	rs = ""
+	rt = ""
+	rd = ""
+	sa = ""
+	if opcode == "add" or opcode == "sub" or opcode == "slt":
+		rs = registers[1]
+		rt = registers[2]
+		rd = registers[0]
+		sa = "0"
+	elif opcode == "sll" or opcode == "srl":
+		rs = "$zero"
+		rt = registers[1]
+		rd = registers[0]
+		sa = registers[2]
+	else:
+		return False
+	
+	i = 1
+	# Cover rs, rt, rd
+	for register in [rs, rt, rd]:
+		number = 0
+		if register == "":
+			i += 1
+			continue
+		
+		if not ("$" in register):
+			
+			print(register)
+			return False
+		register = register.replace("$", "")
+		
+		if register in REGISTER_LISTS:
+			number = REGISTER_LISTS.index(register)
+		elif register.isnumeric():
+			number = int(register)
+		else:
+			return False
+		
+		if number > 31:
+			return False
+		template[i] = int_to_binary(5, number)
+		
+		i += 1
+	
+	# Cover sa
+	if not sa.isnumeric():
+		return False
+	number = int(sa)
+	if number >= 32 or number < 0:
+		return False
+	binary = int_to_binary(5, number)
+	if len(binary) != 5:
+		return False
+	template[4] = binary
+	
+	return True
+	
+def parse_itype(opcode, template, registers):
+	# opcode: string of opcode
+	# template: list type
+	# registers: list of registers 
+	rt = ""
+	rs = ""
+	offset = ""
+	if opcode == "addi" or opcode == "beq" or opcode == "bne":
+		rt = registers[0]
+		rs = registers[1]
+		offset = registers[2]
+	elif opcode == "lw" or opcode == "sw":
+		rt = registers[0]
+		rsoffset = registers[1].split("(")
+		offset = rsoffset[0]
+		rs = rsoffset[1].replace(")", "")
+	else:
+		return False
+	
+	i = 1
+	for register in [rs, rt]:
+		number = 0
+		if not ("$" in register):
+			return False
+		register = register.replace("$", "")
+		if register in REGISTER_LISTS:
+			number = REGISTER_LISTS.index(register)
+		elif register.isnumeric():
+			number = int(register)
+		else:
+			return False
+
+		if number > 31:
+			return False
+		template[i] = int_to_binary(5, number)
+		
+		i += 1
+	
+	# Parse offset
+	number = 0
+	if offset.replace("-", "").isnumeric():
+		number = int(offset)
+	else:
+		return False
+	
+	binary = int_to_binary(16, number)
+	if len(binary) != 16:
+		return False
+	template[3] = binary
+	
+	return True
+
+def int_to_binary(size, number):
+	if number < 0:
+		number = (1 << size) + number
+	formatting = '{:0%ib}' % size
+	return formatting.format(number)
 
 def main():
-	
+	instring = "sub  $31,$2,$8"
+	parse_instruction(instring)
 
 if __name__ == "__main__":
 	main()
